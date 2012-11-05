5646ccdf87709362db481244a35c92b57622f13a
Winston Tsang <rubyconsumer@rubyconsumer.com>
2012-11-05 10:04:57 -0500

    [748] Clicking Anywhere Outside Notifications Window Should Close Notification Window.

--- app/assets/javascripts/notifications/notification_info.js
+++ app/assets/javascripts/notifications/notification_info.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
   $('.notification-container .dropdown-toggle').click(function(){
-
     $.ajax({
       type: "post",
       url: "/notifications/viewed",
@@ -9,10 +8,14 @@ $(document).ready(function() {
         $('.notification-info').html("0");
         $('.notification-main').addClass('notification-inactive')
         $('.notification-main').removeClass('notification-active')
-
       }
     });
+  });
 
+  $('html').click(function (e) {
+    if ($(e.target).parents('.notification-container').length == 0) {
+      $('.notification-container .dropdown-menu').slideUp('ease');
+    }
   });
 
 });
