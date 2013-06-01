b2015ec9f84a7d82a3a8ee22c9dcdd78debfe554
Winston Tsang <rubyconsumer@rubyconsumer.com>
2013-06-01 10:05:42 -0400

    [869] Ability to Receive a Notification of a Comment on a Reflection

--- app/models/reflection_comment.rb
+++ app/models/reflection_comment.rb
@@ -2,9 +2,13 @@ class ReflectionComment < ActiveRecord::Base
   belongs_to :reflection
   belongs_to :person
   validates_presence_of :body
-  
+
   def conversation_id
     try(:reflection).try(:conversation_id)
   end
-  
+
+  def title
+    try(:reflection).try(:title)
+  end
+
 end
