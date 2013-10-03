77b649728e11da79df7da3ae7b3c24599640d768
Kyle Balderson <kyle@fkbalderson.com>
2013-10-03 18:50:50 -0400

    see all participants

--- public/conversation_pages/conversation.js
+++ public/conversation_pages/conversation.js
@@ -4,6 +4,20 @@
     $("#recent-activity").sticky({
       topSpacing:167
     });
-    
+    $("#participants .see-all").click(function(event) {
+      this.participantCount = $("#participants").find(".participant").length;
+      event.preventDefault();
+      if (this.participantCount < 6) {
+        return false;
+      }
+      $("#participants").toggleClass("expanded");
+      if ($("#participants").hasClass("expanded")) {
+        console.log(this.participantCount / 6);
+
+        $(".participants").css({
+          height: (55 * Math.ceil(this.participantCount / 6)),
+        });
+      }
+    });
   });
 })(jQuery)
