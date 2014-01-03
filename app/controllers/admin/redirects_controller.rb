bcda98ce839ab4e1543e9ddb10bc00f3c9aca5c8
Kyle Balderson <kyle@fkbalderson.com>
2014-01-03 15:41:44 -0500

    updates to redirects

--- app/controllers/admin/redirects_controller.rb
+++ app/controllers/admin/redirects_controller.rb
@@ -1,7 +1,23 @@
 class Admin::RedirectsController < Admin::DashboardController
 
   def index
+    @new_redirect = Redirect.new
     @redirects = Redirect.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+
+  def destroy
   end
 
 end
--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -1,2 +1,3 @@
 class Redirect < ActiveRecord::Base
+  attr_accessible :path, :destination
 end
