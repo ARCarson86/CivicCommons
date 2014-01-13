4438f4fdb605b4d833c55a236fd48cbcdcc49427
Mike Burkle <Michael.burkle@lvlsvn.com>
2014-01-13 09:26:37 -0500

    Model and Controller additions to Reports

--- app/controllers/admin/reports_crontroller.rb
+++ app/controllers/admin/reports_crontroller.rb
@@ -2,9 +2,14 @@ class Admin::ReportsController < Admin::DashboardController
 require 'csv'
 
 	def member_report
-		format.csv { send_data @records.member_report }
+		format.csv { send_data Report.member_report }
 	end
 
 	def conversation_summary
+		format.csv { send_data Report.conversation_summary }
+	end
+
+	def project_overview
+		format.csv { send_data Report.project_overview }
 	end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -29,25 +29,28 @@ class Report < ActiveRecord::Base
 	end
 
 	def conversation_summary
-    sql = "select
-      c.id as 'Conversation ID',
-      date(c.created_at) as 'Posted On',
-      c.title as 'Title',
-      c.total_visits as 'Visits',
-      date(c.last_visit_date) as 'Last Visit',
-      if(c.staff_pick = 1, 'Y', '') as 'Staff Pick?',
-      (select count(distinct owner) from contributions where conversation_id = c.id) as '# of Participants',
-      (select count(*) from contributions where conversation_id = c.id) as 'Contributions',
-      (select count(*) from surveys where type = 'Vote' and surveyable_type = 'Conversation' and surveyable_id = c.id) as 'Votes',
-      (select count(distinct sr.person_id) from survey_responses as sr join surveys as s on s.id = sr.survey_id where s.surveyable_type = 'Conversation' and s.type = 'Vote' and s.surveyable_id = c.id) as 'Unique Members Voting',
-      (select count(*) from petitions where conversation_id = c.id) as 'Petitions',
-      (select count(ps.id) from petition_signatures as ps join petitions as p on p.id = ps.petition_id where p.conversation_id = c.id) as 'Petition Signatures',
-      (select count(rating_groups.id) from rating_groups join ratings on ratings.rating_group_id = rating_groups.id where rating_groups.conversation_id = c.id and ratings.rating_descriptor_id = 1) as 'Persuasive Ratings',
-      (select count(rating_groups.id) from rating_groups join ratings on ratings.rating_group_id = rating_groups.id where rating_groups.conversation_id = c.id and ratings.rating_descriptor_id = 2) as 'Informative Ratings',
-      (select count(rating_groups.id) from rating_groups join ratings on ratings.rating_group_id = rating_groups.id where rating_groups.conversation_id = c.id and ratings.rating_descriptor_id = 3) as 'Inspiring Ratings',
-      (select count(*) from subscriptions where subscribable_type = 'Conversation' and subscribable_id = c.id) as 'Subscriptions'
-      from conversations as c"
-    @records = ActiveRecord::Base.connection.execute(sql)    
+    CSV.generate(options) do |csv|
+      csv << ["Conversation ID", "Posted On", "Title", "Visits", "Last Visit", "Staff Pick?", "'#' of Participants", "Contributions", "Votes", "Unique Members Voteing", "Petitions", "Petition Signatures", "Persausive Ratings", "Informative Ratings", "Inspiring Ratings", "Subscriptions"]
+        sql = "select
+        c.id as 'Conversation ID',
+        date(c.created_at) as 'Posted On',
+        c.title as 'Title',
+        c.total_visits as 'Visits',
+        date(c.last_visit_date) as 'Last Visit',
+        if(c.staff_pick = 1, 'Y', '') as 'Staff Pick?',
+        (select count(distinct owner) from contributions where conversation_id = c.id) as '# of Participants',
+        (select count(*) from contributions where conversation_id = c.id) as 'Contributions',
+        (select count(*) from surveys where type = 'Vote' and surveyable_type = 'Conversation' and surveyable_id = c.id) as 'Votes',
+        (select count(distinct sr.person_id) from survey_responses as sr join surveys as s on s.id = sr.survey_id where s.surveyable_type = 'Conversation' and s.type = 'Vote' and s.surveyable_id = c.id) as 'Unique Members Voting',
+        (select count(*) from petitions where conversation_id = c.id) as 'Petitions',
+        (select count(ps.id) from petition_signatures as ps join petitions as p on p.id = ps.petition_id where p.conversation_id = c.id) as 'Petition Signatures',
+        (select count(rating_groups.id) from rating_groups join ratings on ratings.rating_group_id = rating_groups.id where rating_groups.conversation_id = c.id and ratings.rating_descriptor_id = 1) as 'Persuasive Ratings',
+        (select count(rating_groups.id) from rating_groups join ratings on ratings.rating_group_id = rating_groups.id where rating_groups.conversation_id = c.id and ratings.rating_descriptor_id = 2) as 'Informative Ratings',
+        (select count(rating_groups.id) from rating_groups join ratings on ratings.rating_group_id = rating_groups.id where rating_groups.conversation_id = c.id and ratings.rating_descriptor_id = 3) as 'Inspiring Ratings',
+        (select count(*) from subscriptions where subscribable_type = 'Conversation' and subscribable_id = c.id) as 'Subscriptions'
+        from conversations as c"
+        @records = ActiveRecord::Base.connection.execute(sql)    
+          csv << @records
 	end
 
 	def individual_project_stats
@@ -103,22 +106,22 @@ class Report < ActiveRecord::Base
 	end
 
 	def project_overview
-    sql = "select i.name as 'Project',
-      c.title as 'Conversation',
-      date(c.created_at) as 'Conversation Posted',
-      (select count(*) from surveys where surveyable_type = 'Conversation' and surveyable_id = c.id and type = 'Vote') as 'Votes',
-      (select count(sso.id) from selected_survey_options as sso join survey_options as so on sso.survey_option_id = so.id join surveys as s on s.id = so.survey_id where s.surveyable_type = 'Conversation' and s.surveyable_id = c.id) as 'Total Vote Responses',
-      (select count(*) from petitions where conversation_id = c.id) as 'Petitions',
-      (select count(*) from reflections where conversation_id = c.id) as 'Reflections',
-      (select count(rc.id) from reflection_comments as rc join reflections as r on r.id = rc.reflection_id where r.conversation_id = c.id) as 'Reflection Comments',
-      (select count(*) from petitions where conversation_id = c.id) as 'Petitions',
-      (select count(*) from petition_signatures as ps join petitions as p on p.id = ps.petition_id where p.conversation_id = c.id) as 'Petition Signatures',
-      (select count(*) from contributions where conversation_id = c.id) as 'Contributions',
-      (select count(*) from subscriptions where subscribable_type = 'Conversation' and subscribable_id = c.id) as 'Subscriptions',    
-      (select count(*) from visits where visitable_type = 'Conversation' and visitable_id = c.id) as 'Visits'
-      from conversations as c
-      join conversations_issues as ci on ci.conversation_id = c.id
-      join issues as i on i.id = ci.issue_id and i.type = 'ManagedIssue'"
-    @records = ActiveRecord::Base.connection.execute(sql)    
+    CSV.generate(options) do |csv|
+      csv << ["Project", "Conversation", "Votes", "Conversation Posted", "Votes", "Total Vote Responses", "Petitions", "Petition Signatures", "Contributions", "Suscriptions", "Visits", "Managed Issue"]
+      sql = "select i.name as 'Project',
+        c.title as 'Conversation',
+        date(c.created_at) as 'Conversation Posted',
+        (select count(*) from surveys where surveyable_type = 'Conversation' and surveyable_id = c.id and type = 'Vote') as 'Votes',
+        (select count(sso.id) from selected_survey_options as sso join survey_options as so on sso.survey_option_id = so.id join surveys as s on s.id = so.survey_id where s.surveyable_type = 'Conversation' and s.surveyable_id = c.id) as 'Total Vote Responses',
+        (select count(*) from petitions where conversation_id = c.id) as 'Petitions',
+        (select count(*) from petition_signatures as ps join petitions as p on p.id = ps.petition_id where p.conversation_id = c.id) as 'Petition Signatures',
+        (select count(*) from contributions where conversation_id = c.id) as 'Contributions',
+        (select count(*) from subscriptions where subscribable_type = 'Conversation' and subscribable_id = c.id) as 'Subscriptions',    
+        (select count(*) from visits where visitable_type = 'Conversation' and visitable_id = c.id) as 'Visits'
+        from conversations as c
+        join conversations_issues as ci on ci.conversation_id = c.id
+        join issues as i on i.id = ci.issue_id and i.type = 'ManagedIssue'"
+        @records = ActiveRecord::Base.connection.execute(sql)    
+          csv << @records   
 	end
 end
