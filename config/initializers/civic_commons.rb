19e2e07b8a97d7743e4ade87bdcfa51729ffc8e5
Kyle Balderson <kyle@fkbalderson.com>
2014-01-28 15:33:47 -0500

    restore civic_commons.rb

--- config/initializers/civic_commons.rb
+++ config/initializers/civic_commons.rb
@@ -6,11 +6,11 @@ if !defined?(Civiccommons::Config)
     class Config
       include Configurator
 
-      #load_config(:file => File.join(Rails.root, "config", "civic_commons.yml"), :environment => Rails.env)
-      #require_config :config => {:email => :default_email}, :description => "default email config"
-      #require_config :config => {:mailer => :intercept}, :description => "mailer intercept config"
-      #default_config :of => {:mailer => :intercept_email}, :to => {:email => :default_email}
-      #default_config :of => {:mailer => :mailchimp}, :to => false
+      load_config(:file => File.join(Rails.root, "config", "civic_commons.yml"), :environment => Rails.env)
+      require_config :config => {:email => :default_email}, :description => "default email config"
+      require_config :config => {:mailer => :intercept}, :description => "mailer intercept config"
+      default_config :of => {:mailer => :intercept_email}, :to => {:email => :default_email}
+      default_config :of => {:mailer => :mailchimp}, :to => false
     end
 
   end
