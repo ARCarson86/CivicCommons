0e9cc9c4d986ec1e7d7f5cb80bfeb61fb233bb5e
Kyle Balderson <kyle@fkbalderson.com>
2014-02-13 12:52:37 -0500

    check for civic_commons.yml

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,9 +1,13 @@
 rails_env = new_resource.environment["RAILS_ENV"]
 
-Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
+if File.exist? "#{release_path}/config/civic_commons.yml"
+  Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
 
-execute "rake assets:precompile" do
-  cwd release_path
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => rails_env
+  execute "rake assets:precompile" do
+    cwd release_path
+    command "bundle exec rake assets:precompile"
+    environment "RAILS_ENV" => rails_env
+  end
+else
+  Chef::Log.info("No civic_commons.yml - Cannot precompile assets for RAILS_ENV=#{rails_env}...")
 end
