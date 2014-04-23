8109c87b475e57e4adf2d7cd24d79083971dbe8b
Mike Burkle <Michael.burkle@lvlsvn.com>
2014-04-23 13:44:44 -0400

    Opp Headers cached

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Civiccommons::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # DO care if the mailer can't send!
   config.action_mailer.delivery_method = :sendmail
