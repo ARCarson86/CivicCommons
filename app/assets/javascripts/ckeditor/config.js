e61cecb365c1d7755a4c7fd9c0b4e69709d7ab84
Mike Burkle <Michael.burkle@lvlsvn.com>
2014-05-21 15:27:39 -0400

    added allowed content

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,9 +2,9 @@
 Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.config.allowedContent = true;
 CKEDITOR.editorConfig = function( config )
 {
+  config.allowedContent = true;
   // Define changes to default configuration here. For example:
   // config.language = 'fr';
   // config.uiColor = '#AADC6E';
