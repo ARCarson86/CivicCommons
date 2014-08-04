0fb06278080ecd85740e08be768aa6062758a95e
Kyle Balderson <kyle@fkbalderson.com>
2014-08-04 14:11:30 -0400

    fix it

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -2,6 +2,14 @@ rails_env = new_resource.environment["RAILS_ENV"]
 
 Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
 
+directory "#{deploy[:deploy_to]}/shared/assets" do
+  group deploy[:group]
+  owner deploy[:user]
+  mode 0775
+  action :create
+  recursive true
+end
+
 execute "rake assets:precompile" do
   cwd release_path
   command "bundle exec rake assets:precompile"
