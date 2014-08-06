8c6a42e64c200ff8dfa0cd2ecd4ec0cada81ef14
Mike Burkle <Michael.burkle@lvlsvn.com>
2014-08-06 14:13:41 -0400

    changed unlink

--- app/controllers/unlink_controller.rb
+++ app/controllers/unlink_controller.rb
@@ -1,8 +1,9 @@
 class UnlinkController < ApplicationController
   def delete
-  	puts "kitten"
   	person = current_person
     person.unlink_from_social(params[:provider])
-    redirect_to edit_user_path(person), notice: "Your #{params[:provider]} account has been unlinked from your profile."
+    respond_to do |format|
+    	format.html { redirect_to edit_user_path(person), notice: "Your #{params[:provider]} account has been unlinked from your profile."}
+    end
   end
 end
