e244c9df60c2c933a7c2e8e63868b7e84ffb397f
Kyle Balderson <kyle@fkbalderson.com>
2014-10-09 10:05:44 -0400

    remove bower initializer absolute path

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ end
 
 module Civiccommons
   class Application < Rails::Application
-    require "/Users/kbalderson/Projects/CivicComons/CivicCommons/config/initializers/bower_rails.rb"
     config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
