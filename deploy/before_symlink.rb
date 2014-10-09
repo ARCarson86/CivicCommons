9f0d8d51dbd64a0c789f2257cf9ec8d568b40252
Kyle Balderson <kyle@fkbalderson.com>
2014-10-09 17:47:30 -0400

    npm install

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -11,6 +11,7 @@ Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
 execute "rake assets:precompile" do
   cwd release_path
   command "bundle exec rake assets:precompile"
+  user 'deploy'
   environment "RAILS_ENV" => rails_env
   only_if { ::File.exists? "#{release_path}/config/civic_commons.yml" }
 end
