9a5ed03755c972f818e30b0a5ad4be0196597f9e
Kyle Balderson <kyle@fkbalderson.com>
2014-10-13 12:55:38 -0400

    initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Civiccommons
   class Application < Rails::Application
     require "#{Rails.root}/config/initializers/bower_rails.rb"
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
