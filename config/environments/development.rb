eca7c6fbd12a60234f0326a7df00ed3ccb49f3c0
Kyle Balderson <kyle@fkbalderson.com>
2014-10-15 15:00:06 -0400

    add angular settings to staging env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,6 @@ Civiccommons::Application.configure do
   redis_config = YAML.load_file(Rails.root.join('config/redis.yml'))
   config.cache_store = :redis_store, "redis://#{redis_config[Rails.env]["host"]}:#{redis_config[Rails.env]["port"]}/0/cache"
 
+  config.angular_templates.ignore_prefix = ['embed/ng/templates/', 'templates']
+
 end
