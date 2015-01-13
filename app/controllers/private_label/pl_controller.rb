14da869188a32c4d46c954246177beec1f9839f0
Kyle Balderson <kyle@fkbalderson.com>
2015-01-13 12:05:43 -0500

    make private label a concern

--- app/controllers/private_label/pl_controller.rb
+++ app/controllers/private_label/pl_controller.rb
@@ -1,13 +1,3 @@
 class PrivateLabel::PlController < ActionController::Base
-	before_filter :enable_swayze
-
-	alias_method :current_user, :current_person
-
-	layout 'private_label/layouts/application'
-
-	protected
-	def enable_swayze
-    find_by = request.subdomains.length > 1 ? { namespace: request.subdomains.first } : { domain: request.host }
-    @swayze = ::Swayze.new(find_by)
-  end
+  include PrivateLabelControllerConcern
 end
