f30f1c064c67ba27f276377a1cccab2b9f982c6d
Kyle Balderson <kyle@fkbalderson.com>
2015-01-13 15:33:41 -0500

    updates to layout and indentation

--- app/concerns/private_label_controller_concern.rb
+++ app/concerns/private_label_controller_concern.rb
@@ -1,15 +1,15 @@
 module PrivateLabelControllerConcern
   extend ActiveSupport::Concern
 
-	def enable_swayze
-    find_by = request.subdomains.length > 1 ? { namespace: request.subdomains.first } : { domain: request.host }
-    @swayze = ::Swayze.new(find_by)
-  end
-
   included do
     before_filter :enable_swayze
     alias_method :current_user, :current_person
     layout 'private_label/layouts/application'
   end
 
+  protected
+	def enable_swayze
+    find_by = request.subdomains.length > 1 ? { namespace: request.subdomains.first } : { domain: request.host }
+    @swayze = ::Swayze.new(find_by)
+  end
 end
