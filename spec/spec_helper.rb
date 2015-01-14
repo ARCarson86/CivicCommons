24e6bddf059eb6756495c06fc61f8104b99d648b
Dana Kashubeck <dana.kashubeck@lvlsvn.com>
2015-01-14 14:08:00 -0500

    Detect double-loading spec_helper
    This can be removed once we are sure that this is not happening.
    However, the specs run very slowly, which could indicate double-loading
    the environment.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -86,6 +86,29 @@ RSpec.configure do |config|
   end
 end
 
+# figure out where we are being loaded from
+if $LOADED_FEATURES.grep(/spec\/spec_helper\.rb/).any?
+  begin
+    raise "foo"
+  rescue => e
+    puts <<-MSG
+  ===================================================
+  It looks like spec_helper.rb has been loaded
+  multiple times. Normalize the require to:
+
+    require "spec/spec_helper"
+
+  Things like File.join and File.expand_path will
+  cause it to be loaded multiple times.
+
+  Loaded this time from:
+
+    #{e.backtrace.join("\n    ")}
+  ===================================================
+    MSG
+  end
+end
+
 ##
 # Convenience method to provide the content of a specific 
 # fixture file in a test.
