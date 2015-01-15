df45c2ab1bc55f1b3d52d8e8d4b01f87d66a5d4f
Kyle Balderson <kyle@fkbalderson.com>
2015-01-15 16:09:09 -0500

    move after_save and after_create callbacks to observers

--- app/observers/person_observer.rb
+++ app/observers/person_observer.rb
@@ -1,7 +1,15 @@
 class PersonObserver < ActiveRecord::Observer
 
+
   def after_save(person)
     AvatarService.update_avatar_url_for(person)
+    person.send_welcome_email if person.send_welcome?
+    person.update_newsletter_subscription person.weekly_newsletter_changed?
+  end
+
+  def after_create(person)
+    person.notify_civic_commons
+    person.update_newsletter_subscription true
   end
 
 end
