dc4bba16fb03952569e0fb66137078868e1d0904
Dana Kashubeck <dana.kashubeck@lvlsvn.com>
2015-01-20 17:22:55 -0500

    Remove redundant require

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,6 @@ require 'spec_helper'
 require 'webmock/rspec'
 
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
