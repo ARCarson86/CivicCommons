ca70d566661fb5514d0ed9fc5f87a1be49a13a81
Kyle Balderson <kyle@fkbalderson.com>
2015-01-19 21:47:59 -0500

    remove private_label_administrators table and model

--- app/models/private_label.rb
+++ app/models/private_label.rb
@@ -11,7 +11,6 @@ class PrivateLabel < ActiveRecord::Base
   has_attached_file :main_image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :main_image, :content_type => /\Aimage\/.*\Z/
 
-  has_many :private_label_administrators
   has_many :private_label_people
   has_many :people, through: :private_label_people
   has_many :admins, through: :private_label_people, source: :person, conditions: ['private_label_people.admin = ?', true]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150120023814) do
+ActiveRecord::Schema.define(:version => 20150120023905) do
 
   create_table "actions", :force => true do |t|
     t.integer  "conversation_id"
@@ -588,13 +588,6 @@ ActiveRecord::Schema.define(:version => 20150120023814) do
 
   add_index "petitions", ["conversation_id"], :name => "index_petitions_on_conversation_id"
   add_index "petitions", ["person_id"], :name => "index_petitions_on_person_id"
-
-  create_table "private_label_administrators", :force => true do |t|
-    t.integer  "person_id"
-    t.integer  "private_label_id"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-  end
 
   create_table "private_label_people", :force => true do |t|
     t.integer  "private_label_id"
