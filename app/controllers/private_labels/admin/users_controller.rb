dcd267169c755556e8c882d4f014e0cd52dce473
Dana Kashubeck <dana.kashubeck@lvlsvn.com>
2015-01-21 14:22:13 -0500

    Update controller for PrivateLabels

--- app/controllers/private_labels/admin/users_controller.rb
+++ app/controllers/private_labels/admin/users_controller.rb
@@ -1,18 +1,23 @@
-class PrivateLabel::Admin::UsersController < PrivateLabel::Admin::DashboardController
+module PrivateLabels
+  module Admin
+    class UsersController < DashboardController
 
-	def index
-		@people = @swayze.people
-	end
+      def index
+        @people = @swayze.people
+      end
 
-	def show
-		@person = @swayze.people.find(params[:id])
-	end
+      def show
+        @person = @swayze.people.find(params[:id])
+      end
 
-	def new
-		@person = Person.new()
-	end
+      def new
+        @person = Person.new()
+      end
 
-	def create
-		@person = Person.new(params[:person])
-	end
-end
+      def create
+        @person = Person.new(params[:person])
+      end
+    end
+
+  end # Admin module
+end # PrivateLabels module
