f82422625ad7f54e9402580323f474c2fb080ba0
Kyle Balderson <kyle@fkbalderson.com>
2015-01-22 12:43:38 -0500

    minor updates to search controller

--- app/controllers/private_labels/search_controller.rb
+++ app/controllers/private_labels/search_controller.rb
@@ -1,14 +1,11 @@
-module PrivateLabels
-  class SearchController < PlController
+class PrivateLabels::SearchController < PrivateLabels::PlController
 
-    def results
-      subdomain = request.subdomains.first
-      pl_id = PrivateLabel.where(namespace: subdomain).first.id
-      @search = Sunspot.search(Conversation) do
-        with(:private_label_id, pl_id)
-        keywords(params[:q])
-      end
+  def results
+    swayze = @swayze
+    @search = Sunspot.search(Conversation) do
+      with(:private_label_id, swayze.private_label_id)
+      keywords(params[:q])
     end
   end
 
-end # PrivateLabels module
+end
