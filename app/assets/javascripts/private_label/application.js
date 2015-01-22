cb976be0817b30b447015b467ba7f87f800c523e
Adam Carson <adam.carson@lvlsvn.com>
2015-01-22 14:11:56 -0500

    Coversation moderators carousel

--- app/assets/javascripts/private_label/application.js
+++ app/assets/javascripts/private_label/application.js
@@ -3,6 +3,7 @@
 //= require ./session_status
 //= require bootstrap/modal
 //= require bootstrap/transition
+//= require bootstrap/carousel
 //= require tiny_mce/jquery.tinymce.js
 //= require private_label/pl_editable.js
 //= require private_label/header.js
