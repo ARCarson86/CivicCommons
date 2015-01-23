07571c4b04949768cdd9bea07dd6ded98d5afb37
Dana Kashubeck <dana.kashubeck@lvlsvn.com>
2015-01-23 14:45:10 -0500

    Update controller specs for move from PlController to ApplicationController

--- spec/controllers/private_labels/homepage_controller_spec.rb
+++ spec/controllers/private_labels/homepage_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 module PrivateLabels
   RSpec.describe HomepageController do
-    it { should be_a PlController }
+    it { should be_a PrivateLabels::ApplicationController }
   end
 
 end # PrivateLabels module
