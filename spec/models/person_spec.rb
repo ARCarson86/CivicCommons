81385effaef55870a5819e04dd47f15d0d7b5684
Dana Kashubeck <dana.kashubeck@lvlsvn.com>
2015-01-22 19:50:19 -0500

    Add spec for Person model

---
+++ spec/models/person_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Person do
+  it { should have_many(:private_label_people).dependent(:destroy) }
+  it { should have_many(:private_labels).through(:private_label_people) }
+end
