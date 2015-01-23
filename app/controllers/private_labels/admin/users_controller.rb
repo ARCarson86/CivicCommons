11e66db1d04345a52478a180f5ddec5a8410c4ec
Dana Kashubeck <dana.kashubeck@lvlsvn.com>
2015-01-22 19:52:24 -0500

    Refactor to use a before filter to fetch the person

--- app/controllers/private_labels/admin/users_controller.rb
+++ app/controllers/private_labels/admin/users_controller.rb
@@ -1,21 +1,34 @@
 module PrivateLabels
   module Admin
+
     class UsersController < DashboardController
+
+      before_filter :load_person, only: [:show, :update, :edit]
+      before_filter :create_person, only: [:new, :create]
 
       def index
         @people = @swayze.people
       end
 
       def show
-        @person = @swayze.people.find(params[:id])
       end
 
       def new
-        @person = Person.new()
+      end
+
+      def edit
       end
 
       def create
-        @person = Person.new(params[:person])
+      end
+
+      private ##################################################
+
+      ##
+      # Fetch the person record to act upon using the id passed
+      # in via the params hash and store it in an instance variable.
+      def load_person
+        @person = @swayze.people.find(params[:id])
       end
     end
 
