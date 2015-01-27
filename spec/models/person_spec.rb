00f138d08ccf0dad6ab4af2e6493e524176da48b
Dana Kashubeck <dana.kashubeck@lvlsvn.com>
2015-01-27 09:50:07 -0500

    Accept nested attributes for the private_label_person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Person do
   it { should have_many(:private_label_people).dependent(:destroy) }
   it { should have_many(:private_labels).through(:private_label_people) }
+  it { should accept_nested_attributes_for(:private_label_people) }
 end
