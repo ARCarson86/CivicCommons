27569946c2da905fe6b26573ceeabd915828dc28
Kyle Balderson <kyle@fkbalderson.com>
2015-01-28 14:52:36 -0500

    require the sidebar content

--- app/models/private_labels/sidebar.rb
+++ app/models/private_labels/sidebar.rb
@@ -2,5 +2,7 @@ module PrivateLabels
   class Sidebar < ActiveRecord::Base
     include ActiveModel::ForbiddenAttributesProtection
     belongs_to :private_label
+
+    validates :content, presence: true
   end
 end
