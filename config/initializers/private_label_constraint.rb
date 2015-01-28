0163909e971033be2c3b6884d815b223195bc39a
Kyle Balderson <kyle@fkbalderson.com>
2015-01-28 12:47:15 -0500

    update private label constraint

--- config/initializers/private_label_constraint.rb
+++ config/initializers/private_label_constraint.rb
@@ -1,9 +1,12 @@
 class PrivateLabelConstraint
-  def self.matches? request
-    ["privatelabel", "pl"].include? request.subdomains.last or matching_privatelabel?(request.host)
+  def self.matches?(request)
+    !!load_private_label(request)
   end
 
-  def self.matching_privatelabel?(host)
-    PrivateLabel.where(domain: host).any?
+  def self.load_private_label(request)
+    find_by = { domain: request.host }
+    find_by = { namespace: request.subdomains.first } if (request.subdomains.length > 1 && ["privatelabel", "pl"].include?(request.subdomains.last))
+    Swayze.current_private_label = PrivateLabel.first(conditions: find_by)
+    Swayze.current_private_label
   end
 end
