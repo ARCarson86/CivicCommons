13e51d3eeae5e09f80a2dcece33af6d6cef45c0b
Adam Carson <adam.carson@lvlsvn.com>
2015-01-30 14:08:54 -0500

    Conversation responsive, responsive header

--- app/helpers/private_labels/application_helper.rb
+++ app/helpers/private_labels/application_helper.rb
@@ -1,7 +1,7 @@
 module PrivateLabels
   module ApplicationHelper
     def content_area_column_width_class 
-      content_for?(:sidebar) ? 'col-md-8 col-sm-8 col-xs-12' : 'col-md-12'
+      content_for?(:sidebar) ? 'col-md-8 col-sm-12 col-xs-12' : 'col-md-12'
     end
 
     def page_classes
