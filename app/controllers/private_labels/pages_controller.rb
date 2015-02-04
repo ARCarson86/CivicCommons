ccdff33385e2152f4d1dcda3013b877ff4713c56
Kyle Balderson <kyle@fkbalderson.com>
2015-02-03 19:50:57 -0500

    add meta info for pages

--- app/controllers/private_labels/pages_controller.rb
+++ app/controllers/private_labels/pages_controller.rb
@@ -3,6 +3,11 @@ module PrivateLabels
     load_and_authorize_resource class: 'PrivateLabels::Page'
 
     def show
+      @meta_info = {
+        title: @page.title,
+        keywords: @page.meta_keywords,
+        description: @page.meta_description
+      }
     end
 
   end
