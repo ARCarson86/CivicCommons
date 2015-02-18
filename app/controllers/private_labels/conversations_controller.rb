1c65e74371290b6482ef49073e0645267ec02e95
Kyle Balderson <kyle@fkbalderson.com>
2015-02-18 14:29:44 -0500

    updates to private label conversation summary to include breaks, set meta information properly

--- app/controllers/private_labels/conversations_controller.rb
+++ app/controllers/private_labels/conversations_controller.rb
@@ -1,6 +1,7 @@
 module PrivateLabels
   class ConversationsController < ApplicationController
     before_filter :find_conversation, only: [:show, :edit, :update, :destroy]
+    before_filter :set_meta, only: [:show]
     load_and_authorize_resource
 
     def index
@@ -13,20 +14,21 @@ module PrivateLabels
     def show
       @contributions = @conversation.contributions
       @newContribrution = Contribution.new
-
-      @meta_info = {
-        title: @conversation.page_title,
-        description: @conversation.meta_description,
-        keyswords: @conversation.meta_tags,
-        image_url: @conversation.image.url()
-      }
-
     end
 
     protected
 
     def find_conversation
       @conversation = Conversation.accessible_by(current_ability).find params[:id]
+    end
+
+    def set_meta
+      @meta_info = {
+        title: @conversation.title,
+        description: @conversation.meta_description,
+        keyswords: @conversation.meta_tags,
+        image_url: @conversation.image.url()
+      }
     end
 
   end
