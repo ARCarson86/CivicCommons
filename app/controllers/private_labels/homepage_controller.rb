2811dea40dff22d828121d1b60591f93deaa25f6
Adam Carson <adam.carson@lvlsvn.com>
2015-03-26 15:39:22 -0400

    Shows correct sidebar on homepage and test spec

--- app/controllers/private_labels/homepage_controller.rb
+++ app/controllers/private_labels/homepage_controller.rb
@@ -5,7 +5,7 @@ module PrivateLabels
       @people = Swayze.current_private_label.people
       @conversations = Swayze.current_private_label.conversations.paginate(page: params[:page], per_page: 6)
       @page = Page.home_page.first
-
+      @sidebar = Nokogiri::HTML(Swayze.current_private_label.sidebar.content.html_safe)
       @meta_info = {
         image_url: Swayze.current_private_label.main_image.url
       }
