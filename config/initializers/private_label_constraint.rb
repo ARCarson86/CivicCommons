881956ab0b6952b12f947528cc14aec75a9e4a66
Kyle Balderson <kyle@fkbalderson.com>
2015-04-06 16:34:26 -0400

    allow staging private label testing by not requiring the last part of the subdomain

--- config/initializers/private_label_constraint.rb
+++ config/initializers/private_label_constraint.rb
@@ -5,7 +5,7 @@ class PrivateLabelConstraint
 
   def self.load_private_label(request)
     find_by = { domain: request.host }
-    find_by = { namespace: request.subdomains.first } if (request.subdomains.length > 1 && ["privatelabel", "pl"].include?(request.subdomains.last))
+    find_by = { namespace: request.subdomains.first } if (request.subdomains.length > 1 && ["privatelabel", "pl"].include?(request.subdomains[1]))
     Swayze.current_private_label = PrivateLabel.first(conditions: find_by)
     Swayze.current_private_label
   end
